e60230396f3d46b4a94692ecee1e9e9a76091a02
ChandrimaKabiraj <90623273+ChandrimaKabiraj@users.noreply.github.com>
2022-07-20 08:21:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+var randomNum=Math.floor(Math.random()*6)+1;
+var randomImg="dice" + randomNum + ".png";
+var imgsrc = "images/" + randomImg;
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src",imgsrc);
+
+
+var randomNum2=Math.floor(Math.random()*6)+1;
+var randomImg2="dice" + randomNum2 + ".png";
+var imgsrc2 = "images/" + randomImg2;
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src",imgsrc2);
+
+
+if(randomNum > randomNum2)
+{
+  document.querySelector("h1").innerHTML = "Player 1 wins!";
+}
+else if (randomNum2 > randomNum)
+{
+  document.querySelector("h1").innerHTML = "Player 2 wins!";
+}
+else
+{
+  document.querySelector("h1").innerHTML = "Draw!";
+}
